0b735c47528175de5bf32bcc52387c8da3c700cc
Charles <charles.rosenbauer@gmail.com>
2022-08-15 23:49:15 -0500

    Token struct/enum for lexer

--- src/frontend.h
+++ src/frontend.h
@@ -12,6 +12,47 @@
 	* lexer
 	* parser
 */
+typedef enum{
+	TK_OPN_PAR	=	0x00,
+	TK_OPN_BRK	=	0x01,
+	TK_OPN_BRC	=	0x02,
+	TK_END_PAR	=	0x03,
+	TK_END_BRK	=	0x04,
+	TK_END_BRC	=	0x05,
+	
+	TK_ID		=	0x06,
+	TK_NUM		=	0x07,
+	TK_LCOM		=	0x08,
+	TK_MCOM		=	0x09,
+	
+	TK_ADD		=	0x0a,	//	+
+	TK_SUB		=	0x0b,	//	-
+	TK_MUL		=	0x0c,	//	*
+	TK_DIV		=	0x0d,	//	/
+	TK_MOD		=	0x0e,	//	%
+	TK_EXP		=	0x0f,	//	**
+	
+	TK_AND		=	0x10,	//	&
+	TK_OR		=	0x11,	//	|
+	TK_NOT		=	0x12,	//	!
+	TK_XOR		=	0x13,	//	^
+	
+	TK_LS		=	0x14,	//	<
+	TK_LSE		=	0x15,	//	=<
+	TK_GT		=	0x16,	//	>
+	TK_GTE		=	0x17,	//	>=
+	TK_EQ		=	0x18,	//	=
+	TK_NEQ		=	0x19,	//	!=
+	TK_NCP		=	0x1a	//	<!>
+}TokenType;
+
+typedef struct{
+	TokenType	type;
+	uint32_t	pos, file;
+}Token;
+
+
+
 SymbolTable makeSymbolTable	(int);
 void		printSymbolTable(SymbolTable*, char*);
 
---
+++ src/tokens.c
@@ -0,0 +1,10 @@
+#include "frontend.h"
+#include "type.h"
+
+#include "stdint.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+
+
+
