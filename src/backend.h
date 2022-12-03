61ce2bd634efe05cef0254abafad9bb30b415a45
Charles <charles.rosenbauer@gmail.com>
2022-12-03 17:07:11 -0600

    Fleshing out backend structs a bit more, definition printing

--- src/backend.h
+++ src/backend.h
@@ -2,6 +2,8 @@
 #define __BACKEND_HEADER__
 
 
+#include "stdint.h"
+
 #include "util.h"
 
 
@@ -15,7 +17,8 @@ typedef enum{
 	BK_LIST		= 0x005,
 	BK_TPRD		= 0x006,
 	BK_TSUM		= 0x007,
-	BK_BIND		= 0x008,
+	BK_TFUN		= 0x008,
+	BK_BIND		= 0x009,
 
 	// Statements
 	BK_FORALL	= 0x100,
@@ -37,32 +40,55 @@ typedef enum{
 }BCKind;
 
 typedef struct{
-	uint32_t	a, b, c;
+	uint32_t	a, b;
 	BCKind		kind;
 }Bytecode;
 
+typedef struct{
+	Bytecode*	code;
+	int			fill, size;
+}Definition;
+
 
 typedef struct{
-	uint32_t id;
+	uint32_t	id;
+	uint32_t	def;
 	
 	/*
 		list of fields
 		list of operations
 		list of axioms
 	*/
+	uint32_t*	field;
+	uint32_t*	fldef;
+	
+	uint32_t*	op;
+	uint32_t*	opdef;
+	
+	uint32_t*	axiom;
+	uint32_t*	axdef;
+	
+	int			fct, oct, xct;
 }Model;
 
 typedef struct{
-	uint32_t id;
+	uint32_t	id;
+	uint32_t	def;
 	
 	/*
 		assumed statement
 		implied statement
 		list of expressions
 	*/
+	uint32_t	assumption;
+	uint32_t	implication;
+	uint32_t*	proof;
+	int			pct;
 }Theorem;
 
 typedef struct{
+	Definition*	defs;
+
 	Theorem*	ts;
 	int			tct, tcap;
 	
--- src/engine.c
+++ src/engine.c
@@ -1,1 +1,47 @@
+#include "stdio.h"
+
 #include "backend.h"
+
+
+
+
+
+void printDefinition(Definition d){
+	for(int i = 1; i < d.fill; i++){
+		printf("#%03x | ", i);
+		Bytecode b = d.code[i];
+		switch(b.kind){
+			// Terms, constructors
+			case BK_NIL		: printf("nil\n"									); break;
+			case BK_TERM	: printf("term    #%03x\n"			, b.a			); break;
+			case BK_TYPE	: printf("type    #%03x\n"			, b.a			); break;
+			case BK_NUM		: printf("num     #%03x\n"			, b.a			); break;
+			case BK_PAR		: printf("par     #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_LIST	: printf("list    #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_TPRD	: printf("tprd    #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_TSUM	: printf("tsum    #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_TFUN	: printf("tfun    #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_BIND	: printf("bind    #%03x #%03x\n"	, b.a, b.b		); break;
+
+			// Statements
+			case BK_FORALL	: printf("forall  #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_EXISTS	: printf("exists  #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_AND		: printf("and     #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_OR		: printf("or      #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_NOT		: printf("not     #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_FUNC	: printf("func    #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_APPLY	: printf("apply   #%03x #%03x\n"	, b.a, b.b		); break;
+	
+			// Proof combinators
+			case BK_MATCH	: printf("match   #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_SPLIT	: printf("split   #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_CASE	: printf("case    #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_CONTRA	: printf("contra  #%03x #%03x\n"	, b.a, b.b		); break;
+			case BK_ASSUME	: printf("assume  #%03x\n"			, b.a			); break;
+			case BK_CONJCT	: printf("conjct  #%03x\n"			, b.a			); break;
+			case BK_EQUIV	: printf("equiv   #%03x #%03x\n"	, b.a, b.b		); break;
+			
+			default : printf("???\n"); break;
+		}
+	}
+}
